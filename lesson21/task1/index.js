049afd65e4f118e5a4d8b0a39498de0cb667b670
Your Name <y.y.oleksander@gmail.com>
2022-04-05 01:00:50 +0300

    2

--- lesson21/task1/index.js
+++ lesson21/task1/index.js
@@ -6,7 +6,7 @@ const tasks = [
   { text: "Buy meat", done: true },
 ];
 
-const renderTasks = (tasksList) => {
+export const renderTasks = (tasksList) => {
   const listElem = document.querySelector(".list");
 
   const listItemsElements = tasksList
