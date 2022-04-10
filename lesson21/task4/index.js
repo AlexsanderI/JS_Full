e76510e4f07330cd4a443d7f97c0e4f64e331bd1
Your Name <y.y.oleksander@gmail.com>
2022-04-10 18:54:23 +0300

    051022

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -1,25 +1,25 @@
-export function getTitle() {
+function getTitle() {
   const text = document.querySelector(".title").textContent;
   console.log(text);
   return text;
 }
-// getTitle();
+getTitle();
 
-export function getDescription() {
+function getDescription() {
   const inner = document.querySelector(".about").innerText;
   console.log(inner);
   return inner;
 }
 // getDescription();
 
-export function getPlans() {
+function getPlans() {
   const inner = document.querySelector(".plans").innerHTML;
   console.log(inner);
   return inner;
 }
 // getPlans();
 
-export function getGoal() {
+function getGoal() {
   const outer = document.querySelector(".goal").outerHTML;
   console.log(outer);
   return outer;
--- lesson21/task5/index.js
+++ lesson21/task5/index.js
@@ -1,40 +1,5 @@
-class Order {
-  constructor(price, city, type) {
-    this.id = `${(Math.random() * 1000).toFixed()}`;
-    this.price = price;
-    this.city = city;
-    this.type = type;
-    this.isConfirmed = false;
-    this.dateCreated = new Date();
-  }
-
-  checkPrice() {
-    return this.price > 1000 || false;
-  }
-
-  confirmOrder() {
-    if (this.isConfirmed !== true) {
-      this.isConfirmed = true;
-      this.dateConfirmed = new Date();
-    }
-  }
-
-  isValidType() {
-    return this.type === "Buy" || this.type === "Sell";
-  }
+export function getTitle(text) {
+  const newText = (document.querySelector(".title").textContent = text);
+  return newText;
 }
-
-const order1 = new Order(2, "Kharkov", "Buy", 40);
-order1.checkPrice();
-order1.confirmOrder();
-order1.isValidType();
-console.log(order1);
-
-const order2 = new Order(200, "Odesa", "SeFXGll");
-order2.confirmOrder();
-order2.isValidType();
-console.log(order2);
-
-const order3 = new Order(230, "Kiev", "BuyKD;NM");
-order2.confirmOrder();
-console.log(order3);
+getTitle("New world!");
