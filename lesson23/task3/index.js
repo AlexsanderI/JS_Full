2849e4c8016b43932ace83620b8b8a5540470623
Your Name <y.y.oleksander@gmail.com>
2022-04-28 20:27:29 +0300

    done

--- lesson23/task3/index.js
+++ lesson23/task3/index.js
@@ -2,17 +2,21 @@
 // 2 изменить стиль li помле нажатия на check box(input)
 // 2.1 создать функцию если check box(input) === true  то применить стиль list__item_done к list__item
 //  и input равный по id
+// создать функцию для добавления объекта в массив======
+// 1 с помощью метода push добовляем объект с ключами и свойствами
+//  2 если в обекте не пустая строка то добовляем текст если пустане не добовляем
+//  3 после нажатия удалить текст с поля
 
 const tasks = [
   { text: 'Buy milk', done: false, id: getRandomInt() },
-  { text: 'Pick up Tom from airport', done: true, id: getRandomInt() },
+  { text: 'Pick up Tom from airport', done: false, id: getRandomInt() },
   { text: 'Visit party', done: false, id: getRandomInt() },
-  { text: 'Visit doctor', done: false, id: getRandomInt() },
-  { text: 'Buy meat', done: false, id: getRandomInt() },
+  { text: 'Visit doctor', done: true, id: getRandomInt() },
+  { text: 'Buy meat', done: true, id: getRandomInt() },
 ];
 
 function getRandomInt() {
-  return Math.floor(Math.random() * 100).toFixed();
+  return Math.floor(Math.random() * 100);
 }
 const listElem = document.querySelector('.list');
 
@@ -50,11 +54,8 @@ function doneBox(event) {
   const elId = event.target.dataset.id;
   // const boxId = event.target.checked;
   // console.log(elId);
-  const checkId = tasks.find((el) => {
-    console.log(el.id, elId);
-    el.id === elId;
-  });
-
+  const checkId = tasks.find((el) => Number(el.id) === Number(elId));
+  console.log(checkId.done);
   if (event.target.checked) {
     checkId.done = true;
   } else {
@@ -62,17 +63,24 @@ function doneBox(event) {
   }
   listElem.innerHTML = '';
   renderTasks(tasks);
-  taskInput.value = '';
 }
 listElem.addEventListener('click', doneBox);
-// doneBox();
-// function select(even) {
-//   console.log(even.target.checked);
-// }
 
-// box[0].addEventListener('click', select);
-// box[1].addEventListener('click', select);
-// box[2].addEventListener('click', select);
-// box[3].addEventListener('click', select);
-// box[4].addEventListener('click', select);
-// put your code here
+const pushBtn = document.querySelector('.btn');
+const inputeText = document.querySelector('.task-input');
+
+const addTask = (task) => {
+  if (inputeText.value !== '') {
+    tasks.push({
+      text: inputeText.value,
+      done: false,
+      id: getRandomInt(),
+    });
+  }
+  listElem.innerHTML = '';
+
+  renderTasks(tasks);
+  inputeText.value = '';
+};
+
+pushBtn.addEventListener('click', addTask);
