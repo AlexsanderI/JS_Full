7679f087afd92be0fda473fe2ec832e7c54a9176
Your Name <y.y.oleksander@gmail.com>
2022-05-19 21:01:08 +0300

    done

--- lesson34/task2/index.js
+++ lesson34/task2/index.js
@@ -1,14 +1,14 @@
 const baseUrl = 'https://62840a40a48bd3c40b6a3c4f.mockapi.io/api/v1/users';
 
 function getUsersList() {
-  return fetch(baseUrl).then((response) => response.json());
+  return fetch(`${baseUrl}`).then((response) => response.json());
 }
 
 export function getUserById(userId) {
   return fetch(`${baseUrl}/${userId}`).then((response) => response.json());
 }
 
-function createUser(userData) {
+export function createUser(userData) {
   // put your code here
   return fetch(baseUrl, {
     method: 'POST',
@@ -19,14 +19,14 @@ function createUser(userData) {
   });
 }
 
-function deleteUser(userId) {
+export function deleteUser(userId) {
   // put your code here
   return fetch(`${baseUrl}/${userId}`, {
     method: 'DELETE',
   });
 }
 
-function updateUser(userId, userData) {
+export function updateUser(userId, userData) {
   // put your code here
   return fetch(`${baseUrl}/${userId}`, {
     method: 'PUT',
@@ -43,20 +43,20 @@ function updateUser(userId, userData) {
 //   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
 // });
 
-const newUserData = {
-  email: 'cool@email.com',
-  firstName: 'Iron',
-  lastName: 'Man',
-  age: 42,
-};
+// const newUserData = {
+//   email: 'cool@email.com',
+//   firstName: 'Iron',
+//   lastName: 'Man',
+//   age: 42,
+// };
 
-createUser(newUserData).then(() => {
-  console.log('User created');
-});
+// createUser(newUserData).then(() => {
+//   console.log('User created');
+// });
 
-getUsersList().then((users) => {
-  console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-});
+// getUsersList().then((users) => {
+//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+// });
 // const updatedUserData = {
 //   email: 'new@email.com',
 //   firstName: 'John',
---
+++ lesson34/task3/index.js
@@ -0,0 +1,46 @@
+const baseUrl = 'https://62840a40a48bd3c40b6a3c4f.mockapi.io/api/v1/users';
+
+const emailInput = document.querySelector('#email');
+const nameInput = document.querySelector('div.form-control input[name = name]');
+const passwordInput = document.querySelector(
+  'div.form-control input[name = password]'
+);
+
+const formElem = document.querySelector('.login-form');
+const submitBtm = document.querySelector('.submit-button');
+
+const avalebForm = () => {
+  if (
+    emailInput.reportValidity() &&
+    nameInput.reportValidity() &&
+    passwordInput.reportValidity()
+  ) {
+    submitBtm.disabled = false;
+  }
+};
+
+function createUser(event) {
+  event.preventDefault();
+  const user = {
+    email: emailInput.value,
+    name: nameInput.value,
+    password: passwordInput.value,
+  };
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(user),
+  })
+    .then((res) => res.json)
+    .then(() => {
+      //   console.log(user);
+
+      alert(JSON.stringify(user));
+      formElem.reset();
+    });
+}
+
+formElem.addEventListener('input', avalebForm);
+formElem.addEventListener('submit', createUser);
