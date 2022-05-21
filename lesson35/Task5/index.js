29d23bc6e1b8491a714d9b71cdaf6784e8f58bd5
Your Name <y.y.oleksander@gmail.com>
2022-05-22 00:27:55 +0300

    done

--- lesson35/Task5/index.js
+++ lesson35/Task5/index.js
@@ -1,30 +1,13 @@
-import { showSpinner, hideSpinner } from './src/scripts/spinner.js';
-import { fetchUserData, fetchReposData } from './src/scripts/requests.js';
-import { renderUserData, renderRepos } from './src/scripts/render.js';
+const product = '{"type": "Juse"}';
+const tablet = JSON.stringify({ brend: 'Apple' });
 
-const userNameFormInput = document.querySelector('.name-form__input');
-const showUserBtnElem = document.querySelector('.name-form__btn');
-
-const onSearchUser = () => {
-  showSpinner();
-  const userName = userNameFormInput.value;
-  fetchUserData(userName)
-    .then((userData) => {
-      renderUserData(userData);
-      return userData.repos_url;
-    })
-    .then((url) => fetchReposData(url))
-    .then((reposList) => {
-      renderRepos(reposList);
-      hideSpinner();
-    })
-    .catch((err) => {
-      alert(err.message);
-    })
-    .finally(() => {
-      hideSpinner();
-    });
+const parseUser = (product) => {
+  try {
+    return (product = JSON.parse(product));
+  } catch (el) {
+    return null;
+  }
 };
 
-showUserBtnElem.addEventListener('click', onSearchUser);
-// test
+console.log(parseUser(product));
+console.log(parseUser(tablet));
